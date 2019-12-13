8026b309d6727158f756ed0fdd1db7808d0f23ff
Nicole-J12 <2259666828@qq.com>
2019-12-13 11:32:33 +0800

    element ui 下载完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/router'
+import Vue from "vue";
+import App from "./App.vue";
+//路由
+import router from "./router/router";
+//element
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
   router
-}).$mount('#app')
+}).$mount("#app");
